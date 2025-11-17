a3d1f7a6450a65e2dbd50eef865441cdd526fac8
YarioM <yaroslav.minko@innowise.com>
2025-11-17 15:03:38 +0300

    Init

---
+++ Prohibition Of Cyclic Transitions/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prohibition_Of_Cyclic_Transitions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
